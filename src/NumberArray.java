d85343d09252b672e485ea9e4c521757c7ab9ad8
Daniel Chmielewski <dkchmielewski@gmail.com>
2020-06-28 14:35:17 -0400

    Changed object field Number[] numbers to Double[] numbers, declared int i inside for loop

--- src/NumberArray.java
+++ src/NumberArray.java
@@ -2,21 +2,18 @@ import java.util.Scanner;
 
 public class NumberArray {
     public static void main(String[] args) {
-        Number[] numbers = new Number[10];
+        Double[] numbers = new Double[10];
         Scanner scanner = new Scanner(System.in);
 
-        int i;
-        for (i = 0; i < numbers.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println("Podaj liczbe zmiennoprzecinkowa");
             numbers[i] = scanner.nextDouble();
         }
 
-        for (Number number:numbers) {
-            System.out.println(number);
+        double sum = 0;
+        for (double number : numbers) {
+            sum += (number * number);
+            System.out.println(sum);
         }
-
-
     }
-
-
 }
